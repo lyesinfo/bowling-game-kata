fbac85e0b7358ee18eb52c9e11a00ce3c6b8af6b
chelfi <chelfi.ilyes@outlook.com>
2020-03-29 22:24:38 +0200

    project initialization

---
+++ bowling-game-kata/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bowling_game_kata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
